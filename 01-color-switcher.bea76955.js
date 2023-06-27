bdff0de2b128ed3b5508a0a1966e2a07a50b47be
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-06-27 19:40:04 +0000

    Deploying to gh-pages from @ TetyanaDubchak/goit-js-hw-09@4068cdcb9e3c00c927189e6aebb15956c3ca4982 🚀

---
+++ 01-color-switcher.bea76955.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");let a=null;e.addEventListener("click",(function(e){a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.currentTarget.setAttribute("disabled",!0),r.removeAttribute("disabled")})),r.addEventListener("click",(function(t){clearInterval(a),t.currentTarget.setAttribute("disabled",!0),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.bea76955.js.map
