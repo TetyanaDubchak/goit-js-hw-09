5bd678b4b1b7d3c64a2d560b793ff8a960e0834f
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-06-28 22:15:54 +0300

    ready job

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,7 @@
 
 import Notiflix from 'notiflix';
 
-const submitElem = document.querySelector('button[type=submit]');
-
+const submitElem = document.querySelector('.form');
 const delayElem = document.querySelector('input[name="delay"]');
 const stepElem = document.querySelector('input[name="step"]');
 const amountElem = document.querySelector('input[name="amount"]');
@@ -10,12 +9,13 @@ const amountElem = document.querySelector('input[name="amount"]');
 
 const onSubmitHandler = (evt) => {
   evt.preventDefault();
+  
   let numberOfPromise = 0;
 
   let amountDate = Number(amountElem.value);
   let delayDate = Number(delayElem.value);
-  let stepDate = Number(stepElemElem.value);
-
+  let stepDate = Number(stepElem.value);
+     
     if (delayDate<0||stepDate<0||amountDate<0) {
     return
     }
@@ -24,24 +24,24 @@ const onSubmitHandler = (evt) => {
     numberOfPromise += 1;
     delayDate += stepDate;
 
-       createPromise(position, delay)
-      .then(({ position, delay }) => {
-         Notiflix.Notify.success(`:white_check_mark: Fulfilled promise ${ponumberOfPromisesition} in ${delayDate}ms`);
+       createPromise(numberOfPromise, delayDate)
+      .then(({ numberOfPromise, delayDate }) => {
+         Notiflix.Notify.success(`:white_check_mark: Fulfilled promise ${numberOfPromise} in ${delayDate}ms`);
       })
-      .catch(({ position, delay }) => {
+      .catch(({ numberOfPromise, delayDate }) => {
         Notiflix.Notify.failure(`:x: Rejected promise ${numberOfPromise} in ${delayDate}ms`);
       });
     
   };
 
-  function createPromise(position, delay) {
+  function createPromise(numberOfPromise, delayDate) {
     return new Promise((resolve, reject) => {
       const shouldResolve = Math.random() > 0.3;
       setTimeout(() => {
         if (shouldResolve) {
-         resolve({position, delay})
+         resolve({numberOfPromise, delayDate})
       } else {
-        reject({position, delay})
+        reject({numberOfPromise, delayDate})
       }
       }, delayDate)
     })
